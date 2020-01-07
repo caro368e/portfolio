7849f12691f44c835b75bd4552e8865edde3c3d0
caro368e <55078949+caro368e@users.noreply.github.com>
2020-01-07 21:19:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 window.addEventListener("load", navShow);
 
+
 function navShow() {
     console.log("navShow");
     document.querySelector(".burger").addEventListener("click", burgerClick);
+
+
+    document.querySelector("#logo").classList.add("fade_in");
+    document.querySelector(".nav_links").classList.add("fade_in");
+    document.querySelector(".lead").classList.add("fade_in");
+    document.querySelector("h1").classList.add("fade_in");
 }
 
 function burgerClick() {
     console.log("burgerClick");
     document.querySelector(".nav_links").classList.toggle("nav-active");
+    document.querySelector(".nav_links").classList.toggle("drop");
 }
